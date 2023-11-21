a4d4a770214271c02b6a54bba99636a29eb27a45
eserdeiro <eserdeiro@gmail.com>
2023-11-21 18:21:35 -0300

    animatedo installed

--- lib/presentation/widgets/shared/movies_slideshow.dart
+++ lib/presentation/widgets/shared/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cine_app/domain/entities/movie_entity.dart';
 import 'package:flutter/material.dart';
@@ -11,12 +12,12 @@ class MoviesSlideshow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 200,
+      height: 210,
       width: double.infinity,
       child: Swiper(
-        viewportFraction: 0.8,
+        viewportFraction: 0.7,
         scale: 0.8,
-        autoplay: true,
+        //autoplay: true,
         itemCount: movies.length,
         itemBuilder: (context, index) {
           final movie = movies[index];
@@ -31,10 +32,63 @@ class MoviesSlideshow extends StatelessWidget {
 class _Slide extends StatelessWidget {
 
   final Movie movie;
-  const _Slide({super.key, required this.movie});
+  const _Slide({
+    required this.movie
+    });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final decorationBackground = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black45,
+          blurRadius: 10,
+          offset: Offset(0,10),
+        )
+        ]
+    );
+
+    return  Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decorationBackground,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if(loadingProgress != null){
+                return const _SlideLoadingProgress();
+              } else{
+                return child;
+              }
+            },
+            )
+          ),
+    ));
+  }
+}
+
+class _SlideLoadingProgress extends StatelessWidget {
+  const _SlideLoadingProgress();
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.black12,)),
+          SpinPerfect(
+            infinite: true,
+            child: const Center(
+              child: Icon(Icons.refresh, size: 40),
+              ),
+          )
+      ],
+    );
   }
 }
