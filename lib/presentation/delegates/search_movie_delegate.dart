96df6f02e90b294686002c5387d2f921fcf14924
eserdeiro <eserdeiro@gmail.com>
2023-11-25 06:54:41 -0300

    little changes on delegate

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,20 +1,31 @@
+import 'dart:io';
+
+import 'package:animate_do/animate_do.dart';
+import 'package:cine_app/domain/entities/movie_entity.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   // @override
   // String get searchFieldLabel => 'Search';
-
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [
-      const Text('BuildActions'),
+   return [
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: IconButton(onPressed: () {
+          query = '';
+        }, icon: const Icon(Icons.clear_rounded)),
+      )
     ];
+    
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('BuildLeading');
+    return IconButton(onPressed: () {
+      close(context, null);
+    }, icon: Icon(Platform.isAndroid? Icons.arrow_back_outlined : Icons.arrow_back_ios));
   }
 
   @override
