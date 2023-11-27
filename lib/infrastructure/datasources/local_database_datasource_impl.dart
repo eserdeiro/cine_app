4523d7bf3e40cf2cf9dcb07d241159614402b7f3
eserdeiro <eserdeiro@gmail.com>
2023-11-27 02:17:23 -0300

    open db

--- lib/infrastructure/datasources/local_database_datasource_impl.dart
+++ lib/infrastructure/datasources/local_database_datasource_impl.dart
@@ -1,7 +1,24 @@
 import 'package:cine_app/domain/datasources/local_database_datasource.dart';
 import 'package:cine_app/domain/entities/movie_entity.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 //isarDatasource
 class LocalDatabaseDataSourceImpl extends LocalDatabaseDatasource{
+
+  late Future<Isar> database;
+
+  LocalDatabaseDataSourceImpl(){
+    database = openDB();
+  }
+
+  Future<Isar> openDB() async{
+    final dir = await getApplicationCacheDirectory();
+    if(Isar.instanceNames.isEmpty){
+      return await Isar.open([MovieSchema], directory: dir.path, inspector: true);
+    }
+    return Future.value(Isar.getInstance());
+  }
+
   @override
   Future<bool> isMoviefavorite(int movieId) {
     // TODO: implement isMoviefavorite
