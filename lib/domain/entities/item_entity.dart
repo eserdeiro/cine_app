176a6b7936200c958f4fa485b9826252c368a905
eserdeiro <eserdeiro@gmail.com>
2023-12-07 18:42:15 -0300

    change isar db to hive db

--- lib/domain/entities/item_entity.dart
+++ lib/domain/entities/item_entity.dart
@@ -1,22 +1,38 @@
 
-import 'package:isar/isar.dart';
+import 'package:hive/hive.dart';
+
 part 'item_entity.g.dart';
-@collection
+//@collection
+@HiveType(typeId: 1)
 class ItemEntity {
-  Id? isarId = Isar.autoIncrement;
+  //Id? isarId = Isar.autoIncrement;
+  @HiveField(0)
   final bool adult;
+  @HiveField(1)
   final String backdropPath;
+  @HiveField(2)
   final List<String> genreIds;
+  @HiveField(3)
   final int id;
+  @HiveField(4)
   final String originalLanguage;
+  @HiveField(5)
   final String originalTitle;
+  @HiveField(6)
   final String overview;
+  @HiveField(7)
   final double popularity;
+  @HiveField(8)
   final String posterPath;
+  @HiveField(9)
   final DateTime? releaseDate;
+  @HiveField(10)
   final String title;
+  @HiveField(11)
   final bool video;
+  @HiveField(12)
   final double voteAverage;
+  @HiveField(13)
   final int voteCount;
 
   ItemEntity({
--- lib/infrastructure/datasources/local_database_datasource_impl.dart
+++ lib/infrastructure/datasources/local_database_datasource_impl.dart
@@ -1,66 +1,56 @@
-import 'package:isar/isar.dart';
+import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:cine_app/domain/datasources/local_database_datasource.dart';
 import 'package:cine_app/domain/entities/item_entity.dart';
 //isarDatasource
 class LocalDatabaseDataSourceImpl extends LocalDatabaseDatasource{
+  late Future<Box<ItemEntity>> database;
 
-  late Future<Isar> database;
-
-  LocalDatabaseDataSourceImpl(){
+  LocalDatabaseDataSourceImpl() {
+    Hive.registerAdapter(ItemEntityAdapter());
     database = openDB();
   }
 
-  Future<Isar> openDB() async{
+  Future<Box<ItemEntity>> openDB() async {
     final dir = await getApplicationCacheDirectory();
-    if(Isar.instanceNames.isEmpty){
-      return await Isar.open([ItemEntitySchema], directory: dir.path, inspector: true);
-    }
-    return Future.value(Isar.getInstance());
-  }
+    Hive.init(dir.path);
 
+    final box = await Hive.openBox<ItemEntity>('item_entity_box');
+    return box;
+  }
   //Check each item if it is favorite or not, by id
   @override
-  Future<bool> isItemFavorite(int itemId) async{
-    
-    final isar = await database;
-    final ItemEntity? isItemFavorite = await isar.itemEntitys
-    .filter()
-    .idEqualTo(itemId)
-    .findFirst();
-
-    return isItemFavorite != null;
+  Future<bool> isItemFavorite(int itemId) async {
+    final box = await database;
+     final isItemFavorite = box.values
+        .where((item) {
+          return item.id.toInt() == itemId;
+        } )
+        .toList(); // convert a list to use the isNotEmpty method...
+    return isItemFavorite.isNotEmpty;
   }
+
   //Offset brings the items after x, example 
   //If offset is 10, bring the items after the next 10
-  @override
+@override
   Future<List<ItemEntity>> loadFavoriteItems({int limit = 9, int offset = 0}) async{
-    final isar = await database;
-    return isar.itemEntitys.where()
-    .offset(offset)
-    .limit(limit)
-    .findAll();
-    
+    final box = await database;
+    final items =box.values.skip(offset).take(limit).toList() ;
+    return items;
   }
 
   //We save, update or delete when tap favorite icon
-  @override
-  Future<void> toggleFavorite(ItemEntity item) async{
-    
-    final isar = await database;
-    final favoriteItem = await isar.itemEntitys
-    .filter()
-    .idEqualTo(item.id)
-    .findFirst();
+@override
+Future<void> toggleFavorite(ItemEntity item) async {
+  final box = await database;
 
-    if(favoriteItem != null ){
-      //Delete 
-      //We use the id to use(favoriteMovie.id) because it has a unique id
-      isar.writeTxnSync(() => isar.itemEntitys.deleteSync(favoriteItem.isarId!));
-      return;
-    }
-    //Insert 
-    isar.writeTxnSync(() => isar.itemEntitys.putSync(item));
+  // Check if the item is already a favorite
+  final favoriteItemIndex = box.values.toList().indexWhere((itemDb) => itemDb.id == item.id);
+
+  if (favoriteItemIndex != -1) {
+    box.deleteAt(favoriteItemIndex); // Item is already a favorite, remove it
+  } else {
+    box.add(item); // Item is not a favorite, add it
   }
-
+}
 }
