71ecef5a239b34752f712b58b63b0d964881d704
eserdeiro <eserdeiro@gmail.com>
2024-01-10 20:18:48 -0300

    item screen changes

--- lib/presentation/screens/movies/item_screen.dart
+++ lib/presentation/screens/movies/item_screen.dart
@@ -33,7 +33,8 @@ class ItemScreenState extends ConsumerState<ItemScreen> {
     final item = ref.watch(movieDetailProvider)[widget.itemId];
     if (item == null) {
       return const Scaffold(
-          body: Center(child: CircularProgressIndicator(strokeWidth: 3)),);
+        body: Center(child: CircularProgressIndicator(strokeWidth: 3)),
+      );
     }
     return Scaffold(
       body: CustomScrollView(
@@ -41,9 +42,11 @@ class ItemScreenState extends ConsumerState<ItemScreen> {
         slivers: [
           CustomSliverAppBar(item: item),
           SliverList(
-              delegate: SliverChildBuilderDelegate(
-                  (context, index) => _ItemDetails(item: item),
-                  childCount: 1,),),
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => _ItemDetails(item: item),
+              childCount: 1,
+            ),
+          ),
         ],
       ),
     );
@@ -67,15 +70,17 @@ class CustomSliverAppBarState extends ConsumerState<CustomSliverAppBar> {
     final landscape = size.width > 600;
 
     return SliverAppBar(
-      leading: IconButton(onPressed: () {
-        context.pop();
-      }, icon: Icon(
-        kIsWeb
-            ? Icons.arrow_back_outlined 
-            : Platform.isAndroid
-                ? Icons.arrow_back_outlined
-                : Icons.arrow_back_ios,
-      ),
+      leading: IconButton(
+        onPressed: () {
+          context.pop();
+        },
+        icon: Icon(
+          kIsWeb
+              ? Icons.arrow_back_outlined
+              : Platform.isAndroid
+                  ? Icons.arrow_back_outlined
+                  : Icons.arrow_back_ios,
+        ),
       ),
       centerTitle: true,
       title: Text(widget.item.title),
@@ -84,76 +89,86 @@ class CustomSliverAppBarState extends ConsumerState<CustomSliverAppBar> {
       foregroundColor: Colors.white,
       actions: [
         IconButton(
-            onPressed: () async {
-              await ref
-                  .read(favoriteItemsProvider.notifier)
-                  .toggleFavorite(widget.item);
+          onPressed: () async {
+            await ref
+                .read(favoriteItemsProvider.notifier)
+                .toggleFavorite(widget.item);
 
-              ref.invalidate(isFavoriteProvider(widget.item.id));
+            ref.invalidate(isFavoriteProvider(widget.item.id));
+          },
+          icon: isFavoriteFutureProvider.when(
+            data: (isFavorite) {
+              return isFavorite
+                  ? const Icon(
+                      Icons.favorite_border_outlined,
+                      color: Colors.red,
+                    )
+                  : const Icon(Icons.favorite_border_outlined);
             },
-            icon: isFavoriteFutureProvider.when(
-                data: (isFavorite) {
-                  return isFavorite
-                      ? const Icon(Icons.favorite_border_outlined,
-                          color: Colors.red,)
-                      : const Icon(Icons.favorite_border_outlined);
-                },
-                error: (_, __) => const Icon(Icons.cancel_outlined),
-                loading: () =>
-                    SpinPerfect(child: const Icon(Icons.refresh_outlined)),),),
+            error: (_, __) => const Icon(Icons.cancel_outlined),
+            loading: () =>
+                SpinPerfect(child: const Icon(Icons.refresh_outlined)),
+          ),
+        ),
       ],
-
       flexibleSpace: FlexibleSpaceBar(
-        background: LayoutBuilder(builder: (context, constraints) {
-           final availableHeight = constraints.biggest.height;
+        background: LayoutBuilder(
+          builder: (context, constraints) {
+            final availableHeight = constraints.biggest.height;
 
-          return Stack(
-          children: [
+            return Stack(
+              children: [
+                BackgroundImageItem(
+                  imagePath: landscape
+                      ? widget.item.backdropPath
+                      : widget.item.posterPath,
+                ),
 
-          BackgroundImageItem(
-              imagePath: landscape
-                  ? widget.item.backdropPath
-                  : widget.item.posterPath,),
+                //Center image
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //If the screen is very small, we eliminate the sizesbox and posterpath
+                    if (availableHeight >= 270) const SizedBox(height: 60),
+                    //Center posterpath
+                    if (!landscape && availableHeight >= 270)
+                      MainImageItem(
+                        imagePath: widget.item.posterPath,
+                        height: availableHeight * 0.45,
+                      ),
 
-          //Center image
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+                    const SizedBox(height: 20),
 
-                //If the screen is very small, we eliminate the sizesbox and posterpath
-                if (availableHeight >= 270)
-                const SizedBox(height: 60),
-                //Center posterpath
-                if (!landscape && availableHeight >= 270)
-                  MainImageItem(imagePath: widget.item.posterPath, height: availableHeight * 0.45),
-                    
-                const SizedBox(height: 20),
-              
-                //ReleaseDate
-                if (widget.item.releaseDate?.year != null)
-                  ReleaseDate(releaseDate: widget.item.releaseDate),
-                    
-                //Genres
-                GenresItem(genreIds: widget.item.genreIds),
-                    
-                //Vote average
-                VoteAvergateItem(voteAverage: widget.item.voteAverage),
+                    //ReleaseDate
+                    if (widget.item.releaseDate?.year != null)
+                      ReleaseDate(releaseDate: widget.item.releaseDate),
+
+                    //Genres
+                    GenresItem(genreIds: widget.item.genreIds),
+
+                    //Vote average
+                    VoteAvergateItem(voteAverage: widget.item.voteAverage),
+                  ],
+                ),
               ],
-            ),
-          ],);
-        },
-      ),),
+            );
+          },
+        ),
+      ),
     );
   }
 }
 
 class _ItemDetails extends ConsumerWidget {
   final ItemEntity item;
+
   const _ItemDetails({required this.item});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-        final size = MediaQuery.of(context).size;
+
+
+    final size = MediaQuery.of(context).size;
     final landscape = size.width > 600;
     final cast = ref.watch(castByItemProvider);
     final crew = ref.watch(crewByItemProvider);
@@ -161,66 +176,90 @@ class _ItemDetails extends ConsumerWidget {
     final crewIsNotEmpty = crew.values.any((list) => list.isNotEmpty);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-           const SizedBox(height: 10),
-        LayoutBuilder(
-          builder: (context, constraints) {
-             //Landscape posterpath + overview
-            if (landscape) {
-              return Row(
-                children: [
-
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: MainImageItem(imagePath: item.posterPath, height: 200),
-                  ),
-
-                  Expanded(
-                      child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      children: [
-                          const TitleSubtitle(
-                              title: 'Overview', horizontalPadding: 0,),
-                        const SizedBox(width: 10),
-                        Text(item.overview),
-                      ],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 10),
+          LayoutBuilder(
+            builder: (context, constraints) {
+              //Landscape posterpath + overview
+              if (landscape) {
+                return Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: MainImageItem(
+                        imagePath: item.posterPath,
+                        height: 200,
+                      ),
                     ),
-                  ),),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          children: [
+                            const TitleSubtitle(
+                              title: 'Overview',
+                              horizontalPadding: 0,
+                            ),
+                            const SizedBox(width: 10),
+                            Text(item.overview),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                //Portrait overview without posterpath
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const TitleSubtitle(
+                      title: 'Overview',
+                      horizontalPadding: 0,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(item.overview),
+                  ],
+                );
+              }
+            },
+          ),
 
-                ],
-              );
-            } else {
-            //Portrait overview without posterpath
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const TitleSubtitle(title: 'Overview', horizontalPadding: 0),
-                  const SizedBox(height: 10),
-                  Text(item.overview),
-                ],
-              );
-            }
-          },
-        ),
+          //Actors
+          //Cast view
+          if (castIsNotEmpty)
+            TitleSubtitle(
+              title: 'Cast',
+              horizontalPadding: 0,
+              subtitle: 'Show all',
+              onTapSubtitle: () {
+            // Cambiar el estado de showAll cuando se toque el subtítulo
+              
+              },
+            ),
+          ActorsByItem(
+            itemId: item.id.toString(),
+            actorsByItem: cast,
+            showAll: false,
+          ),
 
-        //Actors
-        //Cast view
-        if (castIsNotEmpty)
-           TitleSubtitle(title: 'Cast', horizontalPadding: 0, subtitle: 'Show all', onTapSubtitle: () {
-            print('hola xd');
-          },),
-        ActorsByItem(
-          itemId: item.id.toString(),
-          actorsByItem: cast,
-        ),
-
-        //Crew view
-        if (crewIsNotEmpty)
-          const TitleSubtitle(title: 'Crew', horizontalPadding: 0),
-        ActorsByItem(itemId: item.id.toString(), actorsByItem: crew),
-        const SizedBox(height: 50),
-      ],),
+          //Crew view
+          if (crewIsNotEmpty)
+            const TitleSubtitle(
+              title: 'Crew',
+              horizontalPadding: 0,
+              subtitle: 'Show all',
+            ),
+          ActorsByItem(
+            itemId: item.id.toString(),
+            actorsByItem: crew,
+            showAll: false,
+          ),
+          const SizedBox(height: 50),
+        ],
+      ),
     );
   }
 }
--- lib/presentation/widgets/shared/item/actors_by_item.dart
+++ lib/presentation/widgets/shared/item/actors_by_item.dart
@@ -2,75 +2,91 @@ import 'package:cine_app/domain/entities/actor_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 class ActorsByItem extends ConsumerWidget {
   final String itemId;
   final Map<String, List<ActorEntity>> actorsByItem;
+  final bool showAll;
   const ActorsByItem({
-    required this.itemId, 
+    required this.itemId,
     required this.actorsByItem,
-    super.key, 
-    });
+    required this.showAll,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // if has no data, show circularprogress
     if (actorsByItem[itemId] == null) {
       return const SizedBox(
-          height: 65,
-          child: Center(child: CircularProgressIndicator(strokeWidth: 3)),);
+        height: 65,
+        child: Center(child: CircularProgressIndicator(strokeWidth: 3)),
+      );
     }
 
     final actors = actorsByItem[itemId]!;
-    return SizedBox(
-      height: 65,
-      child:
-       ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: actors.length,
-        itemBuilder: (context, index) {
-          final actor = actors[index];
-          return Container(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-                //Actor photo
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(50),
-                  child: Image.network(actor.profilePath,
-                      height: 50, width: 50,  fit: BoxFit.cover,),
+    return GridView.builder(
+      padding: EdgeInsets.zero,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 2.5,
+      ),
+      itemCount: showAll ? actors.length : 4,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        final actor = actors[index];
+        return Container(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Actor photo
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network(
+                  actor.profilePath,
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
                 ),
+              ),
 
-                //Name
-                const SizedBox(height: 5),
-                Padding(
+              //Name
+              const SizedBox(height: 5),
+              Expanded(
+                child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 5),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(actor.name,
-                       style: const TextStyle(
-                         fontWeight: FontWeight.w600,),),
+                      Text(
+                        actor.name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
                       const SizedBox(height: 5),
-                      
+
                       //Actor character, if null, show job.
-                      Text((actor.character == null) ? actor.job! : actor.character! ,
-                          maxLines: 1,
-                          style: const TextStyle(
-                            fontSize: 12, overflow: TextOverflow.ellipsis,
-                      ),),
-                      
+                      Text(
+                        (actor.character == null)
+                            ? actor.job!
+                            : actor.character!,
+                        maxLines: 1,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
                     ],
                   ),
                 ),
-                //
-              ],
-            ),
-          );
-        },
-      ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
