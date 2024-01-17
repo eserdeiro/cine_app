7b4a6f8ad258ca8bbb66a3e557f78e08a110f2a5
eserdeiro <eserdeiro@gmail.com>
2024-01-17 03:23:17 -0300

    changes on aspect ratio on actors gridview

--- lib/presentation/widgets/shared/item/actors_by_item.dart
+++ lib/presentation/widgets/shared/item/actors_by_item.dart
@@ -1,3 +1,4 @@
+import 'package:cine_app/config/helpers/orientation.dart';
 import 'package:cine_app/domain/entities/actor_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -15,6 +16,8 @@ class ActorsByItem extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final orientationHelper = OrientationHelper(context);
+    final landscape = orientationHelper.isLandscape;
     // if has no data, show circularprogress
     if (actorsByItem[itemId] == null) {
       return const SizedBox(
@@ -26,65 +29,62 @@ class ActorsByItem extends ConsumerWidget {
     final actors = actorsByItem[itemId]!;
     return GridView.builder(
       padding: EdgeInsets.zero,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 2.5,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: landscape ? 4 : 2,
+        childAspectRatio: landscape ? 3.5 : 2.5,
       ),
       itemCount: showAll ? actors.length : 4,
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
       itemBuilder: (context, index) {
         final actor = actors[index];
-        return Container(
-          padding: const EdgeInsets.all(8),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //Actor photo
-              ClipRRect(
-                borderRadius: BorderRadius.circular(50),
-                child: Image.network(
-                  actor.profilePath,
-                  height: 50,
-                  width: 50,
-                  fit: BoxFit.cover,
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Actor photo
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.network(
+                actor.profilePath,
+                height: 50,
+                width: 50,
+                fit: BoxFit.cover,
+              ),
+            ),
+        
+            //Name
+            const SizedBox(height: 5),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      actor.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+        
+                    //Actor character, if null, show job.
+                    Text(
+                      (actor.character == null)
+                          ? actor.job!
+                          : actor.character!,
+                      maxLines: 1,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-
-              //Name
-              const SizedBox(height: 5),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        actor.name,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w600,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-
-                      //Actor character, if null, show job.
-                      Text(
-                        (actor.character == null)
-                            ? actor.job!
-                            : actor.character!,
-                        maxLines: 1,
-                        style: const TextStyle(
-                          fontSize: 12,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         );
       },
     );
